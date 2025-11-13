9d3fc0a746f87b39c12c7330138d4b10f87ea277
asraful <asrafulislamasf@gmail.com>
2025-11-13 19:33:25 +0600

    Sorting Added

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 
 const app=express()
 const port=process.env.PORT || 3000
@@ -30,14 +30,24 @@ async function run() {
           const eventCollaction=db.collection('event')
 
           app.get('/event', async(req,res)=>{
-             const cursor= eventCollaction.find()
-             const result= await cursor.toArray()
-             res.send(result)
+             const today = new Date();
+             const query = { event_date: { $gte: today } };
+             const cursor = eventCollaction.find(query).sort({ event_date: 1 });
+             const result = await cursor.toArray();
+             res.send(result);
+          })
+
+          app.get('/event-details/:id', async(req,res)=>{
+            const id = req.params.id
+            const query={_id: new ObjectId(id)}
+            const result= await eventCollaction.findOne(query)
+            res.send(result)
           })
 
 
           app.post('/event', async(req,res)=>{
                const data = req.body
+               data.event_date = new Date(data.event_date);
                const result = await eventCollaction.insertOne(data)
                res.send(result)
           })
