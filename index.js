c35636be46abab56f52de680984f5832d368e395
asraful <asrafulislamasf@gmail.com>
2025-11-15 18:16:44 +0600

    Vercel Json Added

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ const uri = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}
          });
 async function run() {
     try{
-          await client.connect();
+          // await client.connect();
 
           const db=client.db('eventDb')
           const eventCollaction=db.collection('event')
@@ -121,7 +121,7 @@ async function run() {
           })
 
      
-           await client.db("admin").command({ ping: 1 });
+          //  await client.db("admin").command({ ping: 1 });
             console.log("Pinged your deployment. You successfully connected to MongoDB!");
     }
     finally{
